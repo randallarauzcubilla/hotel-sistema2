a9a7b4a04fdc13aadf62f0388c36ca31a1b531a4
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2026-03-08 00:06:44 -0600

    feat: PWA configurada

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
